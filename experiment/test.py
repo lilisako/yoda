081ae474a8406ea66459fdb35d31cf45a1e44e5b
Risako <hamarisa0623@chiba-u.jp>
2020-06-11 13:29:08 +0900

    Add get info method

--- experiment/test.py
+++ experiment/test.py
@@ -8,3 +8,7 @@ from yoda import FeatureEngineering
 df = pd.read_csv('../data/titanic/train.csv')
 train, test = train_test_split(df, test_size=0.33, random_state=42)
 train, test = FeatureEngineering.count_encoding(train, test, ['Pclass'])
+train,test = FeatureEngineering.label_encoding(train, test, ['Sex'])
+train,test = FeatureEngineering.fillna(train, test, ['Age'], 'mean')
+FeatureEngineering.info(train)
+FeatureEngineering.categorical_info(train,'Survived', 'Pclass')
--- yoda/FeatureEngineering.py
+++ yoda/FeatureEngineering.py
@@ -10,3 +10,39 @@ def count_encoding(train: pd.DataFrame, test: pd.DataFrame, target: dict):
     train = df[l:].reset_index(drop=True)
     test = df[:l]
     return train,test
+
+def label_encoding(train: pd.DataFrame, test: pd.DataFrame, target: dict):
+    df = pd.concat([train,test],sort=False).reset_index(drop=True)
+    l = len(train)
+    for col in target:
+        df[col] = df[col].astype('category')
+        df['label_enc_'+col] = df[col].cat.codes
+    train = df[l:].reset_index(drop=True)
+    test = df[:l]
+    return train,test
+
+def fillna(train: pd.DataFrame, test:pd.DataFrame, target: dict, mode='mean'):
+    df = pd.concat([train,test],sort=False).reset_index(drop=True)
+    l = len(train)
+    if mode == 'mean':
+        df[target] = df[target].fillna(df.mean())
+    elif mode == 'median':
+        df[target] = df[target].fillna(df.median())
+    elif mode == 'mode':
+        df[target] = df[target].fillna(df.mode())
+    else:
+        return None
+    train = df[l:].reset_index(drop=True)
+    test = df[:l]
+    return train,test
+
+def info(df: pd.DataFrame()):
+    print("===== NULL COUNT =====")
+    print(df.isna().sum())
+    print("===== INFO =====")
+    print(df.info())
+    print("===== DESCRIBE ======")
+    print(df.describe())
+
+def categorical_info(df: pd.DataFrame, target: str, categorical: str):
+    print(df[[target, categorical]].groupby([categorical], as_index=False).mean())
